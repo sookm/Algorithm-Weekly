930bfab50b1dc9e189acc1564f3e8c4ba5c24c97
turi <73997195+sookm@users.noreply.github.com>
2022-03-10 15:47:02 +0900

    Time: 151 ms (79.95%), Space: 47.3 MB (43.59%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,52 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/*
+Initial idea 1
+    1) get string, bring each string using "let i of s"
+    2) use switch condition statements, 
+        for this, elements of s should be integer type 
+        and then plus all of them 
+
+idea 2
+    Use replace() functions and Regular Expression  
+    There is targetStr()
+
+
+var romanToInt = function(s) {
+    const replacedRoman = targetStr.replace(a,b);
+    
+    for (let i of s) {     
+    }
+};
+*/
+
+// Solution -> Use table 
+var romanToInt = function(s) {
+    let table = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        //if the next roman numeral is larger, then we know we have to subtract this number
+        if (table[s[i]] < table[s[i+1]]) {
+            result-=table[s[i]]
+        } 
+        //otherwise, add like normal. 
+        else {
+            result+=table[s[i]]
+        }
+    }
+    return result
+    
+};
+    
